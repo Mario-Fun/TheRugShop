0c60d63bd56ddbde71813a43af02ac995e33b4cc
Alex <alexkfine2@gmail.com>
2022-10-15 10:50:38 -0400

    added valid api key

--- backend/initial_api_test.py
+++ backend/initial_api_test.py
@@ -4,7 +4,10 @@ import requests
 
 url = "https://api.transpose.io/nft/transfers-by-contract-address?contract_address=0x7831729a089df41d7c5bcbd5cebb9d7d131addd3&transfer_category=all&order=asc&limit=10"
 
-headers = {"accept": "application/json"}
+headers = {
+    "accept": "application/json",
+    'X-API-KEY': '7avWNJ6gs97YBcywmr6veXjtm8OTSW1C'
+}
 
 response = requests.get(url, headers=headers)
 
