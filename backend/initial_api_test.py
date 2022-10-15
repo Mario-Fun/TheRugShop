ac8b386df500f9b53e0c33d552470ad5d1ce8e67
Alex <alexkfine2@gmail.com>
2022-10-15 10:47:11 -0400

    init commit

---
+++ backend/initial_api_test.py
@@ -0,0 +1,11 @@
+import math
+import requests
+
+
+url = "https://api.transpose.io/nft/transfers-by-contract-address?contract_address=0x7831729a089df41d7c5bcbd5cebb9d7d131addd3&transfer_category=all&order=asc&limit=10"
+
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
