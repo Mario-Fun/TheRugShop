be99def854612b937b155ac25f93369065402511
Alex <alexkfine2@gmail.com>
2022-10-15 11:38:39 -0400

    Cleaned up code

--- backend/initial_api_test.py
+++ backend/initial_api_test.py
@@ -1,14 +1,32 @@
 import math
 import requests
 
-
-url = "https://api.transpose.io/nft/transfers-by-contract-address?contract_address=0x7831729a089df41d7c5bcbd5cebb9d7d131addd3&transfer_category=all&order=asc&limit=10"
-
-headers = {
+HEADERS = {
     "accept": "application/json",
     'X-API-KEY': '7avWNJ6gs97YBcywmr6veXjtm8OTSW1C'
 }
 
-response = requests.get(url, headers=headers)
+TEST_COLLECTION = "0x7831729a089df41d7c5bcbd5cebb9d7d131addd3"
 
-print(response.text)
+
+def get_transfer(contract_addy):
+    transfer_url = "https://api.transpose.io/nft/transfers-by-contract-address?contract_address=" + contract_addy + "&transfer_category=all&order=asc&limit=10"
+
+    response = requests.get(transfer_url, headers=HEADERS)
+
+    print(response.text)
+
+    return response.text
+
+
+def get_sales(contract_addy):
+    sale_url = "https://api.transpose.io/nft/sales-by-contract-address?contract_address=" + contract_addy + "&order=asc&limit=10"
+
+    response = requests.get(sale_url, headers=HEADERS)
+
+    print(response.text)
+
+    return response.text
+
+
+get_sales(TEST_COLLECTION)
